9eb8c6ecbded9ba6dd79b3de307a7784bb509762
git init <your-email@example.com>
2025-06-19 13:17:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  const hamburger = document.querySelector(".hamburger");
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("open");
+}
